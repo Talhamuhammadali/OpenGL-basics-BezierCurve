9de0bef7c4aa2be8adb8ec1ef2f7113a403c1a8c
Talhamuhammadali <talha_muhammadali@hotmail.com>
2023-12-15 14:52:48 +0500

    updated

--- Basic_Bezier_Curves/src/main.cpp
+++ Basic_Bezier_Curves/src/main.cpp
@@ -8,16 +8,26 @@ int main(){
         std::cout << "Failed to initialize GLFW" << std::endl;
         return -1;
     }
-    
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     window = glfwCreateWindow(640, 480, "FirstWindow", NULL, NULL);
+
     if (!window){
         std::cout << "Failed to Creat Widow" << std::endl;
         return -1;
     }
     glfwMakeContextCurrent(window);
+    if (!gladLoadGL()){
+        std:: cout << "Couldnt load OpenGL" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+
     while (!glfwWindowShouldClose(window))
     {
         glfwPollEvents();
+        glClear(GL_COLOR_BUFFER_BIT);
         glfwSwapBuffers(window);
     }
     glfwTerminate();
