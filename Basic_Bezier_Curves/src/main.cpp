84d6ac004269678089e21318587282ff1f8e3bff
Talhamuhammadali <talha_muhammadali@hotmail.com>
2023-12-15 13:13:26 +0500

    Added cmake and main.cpp

---
+++ Basic_Bezier_Curves/src/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+// #include "glad_gl.h"
+#include <GLFW/glfw3.h>
+
+int main(){
+        GLFWwindow* window;
+    if (!glfwInit()){
+        std::cout << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+    
+    window = glfwCreateWindow(640, 480, "FirstWindow", NULL, NULL);
+    if (!window){
+        std::cout << "Failed to Creat Widow" << std::endl;
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+    glfwTerminate();
+    return 0;
+}
