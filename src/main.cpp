9c0e6c6d10027b6d02a69be908cb9309dbf3d7bc
Talhamuhammadali <talha_muhammadali@hotmail.com>
2023-12-18 17:31:31 +0500

    basic window popup

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,29 @@
 
 int main(){
     std::cout<< "Testing 123" << std::endl;
+    GLFWwindow* window;
+    
+    if(!glfwInit()){
+        std::cout << "Couldnt initialize GLFW3" << std::endl;
+        return -1;
+    }
+    std::cout<< "Creating Window" << std::endl;
+    window = glfwCreateWindow(1366, 720, "OpenGL_Basics", NULL, NULL);
+
+    if(!window){
+        std::cout << "No window created" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+
+    while(!glfwWindowShouldClose(window)){
+        
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
